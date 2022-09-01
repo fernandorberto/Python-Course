710b1dbbb48a8a3b639c8053a6ab6629c67e9ce0
Fernando Berto <49960033+fernandorberto@users.noreply.github.com>
2022-09-01 17:03:04 -0300

    Create data_nascimento.py

---
+++ data_nascimento.py
@@ -0,0 +1,7 @@
+print("Insira a data de seu nascimento:\n")
+
+dia = input("Informe o dia:")
+mes = input("Informe o mês:")
+ano = input("Informe o ano:")
+
+print('Sua data de nascimento é ' +dia+ '/' +mes+ '/' +ano)
